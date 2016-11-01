9d4af05c0af5c0c6a5b869df6bf59740adf35117
Kelsey Nichols <kelseynichols2@gmail.com>
2016-10-31 20:06:35 -0500

    video on home

--- client/controllers.js
+++ client/controllers.js
@@ -1,16 +1,44 @@
 angular.module('DroneApp.controllers', [])
-.controller('WelcomeController', ['$scope', function($scope) {
-    console.log('Welcome!')
-}])
-.controller('InfoController', ['$scope', function($scope) {
+    .controller('WelcomeController', ['$scope', function ($scope) {
+        console.log('Welcome!')
 
-}])
-.controller('PastWorkController', ['$scope', function($scope) {
+        var vid = document.getElementById("bgvid");
+        var pauseButton = document.querySelector("#polina button");
 
-}])
-.controller('LogInController', ['$scope', function($scope) {
+        function vidFade() {
+            vid.classList.add("stopfade");
+        }
 
-}])
-.controller('AccountController', ['$scope', function($scope) {
+        vid.addEventListener('ended', function () {
+            // only functional if "loop" is removed 
+            vid.pause();
+            // to capture IE10
+            vidFade();
+        });
 
-}])
+
+        pauseButton.addEventListener("click", function () {
+            vid.classList.toggle("stopfade");
+            if (vid.paused) {
+                vid.play();
+                pauseButton.innerHTML = "Pause";
+            } else {
+                vid.pause();
+                pauseButton.innerHTML = "Paused";
+            }
+        })
+
+
+    }])
+    .controller('InfoController', ['$scope', function ($scope) {
+
+    }])
+    .controller('PastWorkController', ['$scope', function ($scope) {
+
+    }])
+    .controller('LogInController', ['$scope', function ($scope) {
+
+    }])
+    .controller('AccountController', ['$scope', function ($scope) {
+
+    }])
