7f90051a1373b5f4f339968e609a11312e66b250
gperanich <gperanich@gmail.com>
2016-10-31 20:11:53 -0500

    updated login controller

--- client/controllers.js
+++ client/controllers.js
@@ -8,8 +8,25 @@ angular.module('DroneApp.controllers', [])
 .controller('PastWorkController', ['$scope', function($scope) {
 
 }])
-.controller('LogInController', ['$scope', function($scope) {
-
+.controller('LogInController', ['$scope', 'UserService', '$location', function($scope, UserService, $location) {
+    UserService.me().then(function(success) {
+        redirect();
+    });
+    function redirect() {
+        var dest = $location.search().p;
+        if (!dest) {
+            dest='/';
+        }
+        $location.path(dest).search('p', null).replace();
+    }
+    $scope.login = function() {
+        UserService.login($scope.email, $scope.password)
+        .then(function() {
+            redirect();
+        }, function(err) {
+            onsole.log(err);
+        });
+    }
 }])
 .controller('AccountController', ['$scope', function($scope) {
 
