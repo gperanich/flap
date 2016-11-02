e753bc9af2dc54ee467a581cf2ba08b7779e4f52
Kelsey Nichols <kelseynichols2@gmail.com>
2016-11-01 20:56:24 -0500

    css stuff

--- client/controllers.js
+++ client/controllers.js
@@ -1,65 +1,71 @@
 angular.module('DroneApp.controllers', [])
     .controller('WelcomeController', ['$scope', function ($scope) {
-            console.log('Welcome!')
+        console.log('Welcome!')
 
-            var vid = document.getElementById("bgvid");
+        var vid = document.getElementById("bgvid");
 
     }])
-    .controller('LoginController', ['$scope', 'UserService', '$location', function($scope, UserService, $location) {
-        console.log('login page loaded');
-        UserService.me().then(function(success) {
+    .controller('LoginController', ['$scope', 'UserService', '$location', function ($scope, UserService, $location) {
+        UserService.me().then(function (success) {
             redirect();
         });
         function redirect() {
             var dest = $location.search().p;
             if (!dest) {
-                dest='/';
+                dest = '/';
             }
             $location.path(dest).search('p', null).replace();
         }
-        $scope.login = function() {
+        $scope.login = function () {
             console.log('clicked login');
             UserService.login($scope.email, $scope.password)
-            .then(function(success) {
-                console.log('logged in!');
-                redirect();
-            }, function(err) {
-                console.log(err);
+                .then(function (success) {
+                    console.log('logged in!');
+                    redirect();
+                }, function (err) {
+                    console.log(err);
+                });
+        }
+
+        $(document).ready(function () {
+           $(".user-creator-login").delay(75).animate({ opacity: 1 }, 200)
+
+
+        })
+
+    }])
+    .controller('AccountController', ['$scope', 'Buildings', 'UserService', 'Routes', function ($scope, Buildings, UserService, Routes) {
+        $scope.hideRoutes = true;
+        $scope.hideDetails = true;
+        $scope.showDetails = function () {
+            console.log('clicked to see building details');
+            $scope.hideDetails = !$scope.hideDetails;
+        };
+        $scope.showRoutes = function () {
+            console.log('clicked to see routes');
+            $scope.hideRoutes = !$scope.hideRoutes;
+        };
+
+        var user = UserService.me().then(function (success) {
+            user = success.id;
+            $scope.buildings = Buildings.filter({ userid: success.id });
+            $scope.routes = Routes.filter({ userid: success.id });
+            console.log($scope.routes);
+        });
+
+        $scope.createBuilding = function () {
+            var buildingData = {
+                userid: user,
+                height: $scope.height,
+                width: $scope.width,
+                length: $scope.length
+            };
+            console.log(user);
+            var building = new Buildings(buildingData);
+            building.$save(function (success) {
+                console.log(success);
             });
         }
-    }])
-    .controller('AccountController', ['$scope', 'Buildings', 'UserService', 'Routes', function($scope, Buildings, UserService, Routes) {
-            $scope.hideRoutes = true;
-            $scope.hideDetails = true;
-            $scope.showDetails = function() {
-                console.log('clicked to see building details');
-                $scope.hideDetails = !$scope.hideDetails;
-            };
-            $scope.showRoutes = function() {
-                console.log('clicked to see routes');
-                $scope.hideRoutes = !$scope.hideRoutes;
-            };
-
-            var user = UserService.me().then(function(success) {
-                user = success.id;
-                $scope.buildings = Buildings.filter({ userid: success.id });
-                $scope.routes = Routes.filter({ userid: success.id });
-                console.log($scope.routes);
-            });
-
-            $scope.createBuilding = function() {
-                var buildingData = {
-                    userid: user,
-                    height: $scope.height,
-                    width: $scope.width,
-                    length: $scope.length
-                };
-                console.log(user);
-                var building = new Buildings(buildingData);
-                building.$save(function(success) {
-                    console.log(success);
-                });
-            }
 
 
     }])
@@ -70,11 +76,11 @@ angular.module('DroneApp.controllers', [])
 
     }])
 
-    .controller('ContactController', ['$scope', function($scope) {
-        
+    .controller('ContactController', ['$scope', function ($scope) {
+
     }])
-    .controller('RegisterController', ['$scope', 'Users', '$location', function($scope, Users, $location) {
-        $scope.newUser = function() {
+    .controller('RegisterController', ['$scope', 'Users', '$location', function ($scope, Users, $location) {
+        $scope.newUser = function () {
             var userData = {
                 firstname: $scope.firstname,
                 lastname: $scope.lastname,
@@ -82,13 +88,17 @@ angular.module('DroneApp.controllers', [])
                 password: $scope.password
             };
             var user = new Users(userData);
-            user.$save(function(success) {
+            user.$save(function (success) {
                 $location.url('/');
             });
         }
+        
+        $(document).ready(function () {
+           $(".user-creator").delay(75).animate({ opacity: 1 }, 200)
+        })
 
     }])
-    
+
             // function vidFade() {
             //     vid.classList.add("stopfade");
             // }
