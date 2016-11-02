000c1dbc298ca360d2ade0f2ba8a3307a9e0a189
gperanich <gperanich@gmail.com>
2016-11-02 15:29:23 -0500

    deleted viewbuilding directive

--- client/directives.js
+++ client/directives.js
@@ -13,53 +13,6 @@ angular.module('DroneApp.directives', [])
             }]
         };
     })
-    .directive('viewBuilding', function() {
-        return {
-            templateUrl: 'directives/viewbuilding.html',
-            restrict: 'E',
-            controller: ['$scope', function($scope) {
-                console.log($scope.building);
-                $(document).ready(function() {
-                    console.log('in the jquery handler');
-                    var createShape = function() {
-                        console.log('creating shape');
-                        var canvas = $('#shape-div');
-                        var Shape = function(width, height) {
-                            this.width = width;
-                            this.height = height;
-                        }
-                        Shape.prototype.draw = function() {
-                            this.div = $('<div></div>');
-                            this.div.css({
-                                position: "relative",
-                                background: "rgba(255,0,0,0.5)",
-                                width: (this.width * 10) + "px",
-                                height: (this.height * 10) + "px",
-                                top: "50px",
-                                left: "50px"
-                            });
-                            canvas.append(this.div);
-                        }
-                        var Rectangle = function(width, height) {
-                            Shape.call(this, width, height);
-                            this.cssClass = 'new-rectangle';
-                            this.draw();
-                        }
-                        Rectangle.prototype = Object.create(Shape.prototype);
-                        Rectangle.prototype.constructor = Rectangle;
-                        function createRectangle() {
-                            console.log('drawing building');
-                            new Rectangle($scope.building.width, $scope.building.length);
-                        }
-                        createRectangle();
-                    }
-
-                    createShape();
-                
-                })
-            }]
-        }
-    })
     .directive('createBuilding', function() {
         return {
             templateUrl: 'directives/createbuilding.html',
@@ -120,6 +73,50 @@ angular.module('DroneApp.directives', [])
                 $scope.clearRoute = function() {
                     console.log('clicked claer route');
                     $scope.routeCommands = [];
+                }
+
+                $scope.changeBuilding = function() {
+                    console.log($scope.selectedBuilding);
+                    $(document).ready(function() {
+                        console.log('in the jquery handler');
+                        $('.routebuilding-shape').remove();
+                        var createShape = function() {
+                            console.log('creating shape');
+                            var canvas = $('.buildingroutediv');
+                            var Shape = function(width, height) {
+                                this.width = width;
+                                this.height = height;
+                            }
+                            Shape.prototype.draw = function() {
+                                this.div = $('<div></div>');
+                                this.div.addClass('routebuilding-shape');
+                                this.div.css({
+                                    position: "relative",
+                                    background: "rgba(255,0,0,0.5)",
+                                    width: (this.width * 10) + "px",
+                                    height: (this.height * 10) + "px",
+                                    top: "50px",
+                                    left: "50px"
+                                });
+                                canvas.append(this.div);
+                            }
+                            var Rectangle = function(width, height) {
+                                Shape.call(this, width, height);
+                                this.cssClass = 'new-rectangle';
+                                this.draw();
+                            }
+                            Rectangle.prototype = Object.create(Shape.prototype);
+                            Rectangle.prototype.constructor = Rectangle;
+                            function createRectangle() {
+                                console.log('drawing building');
+                                new Rectangle($scope.selectedBuilding.width, $scope.selectedBuilding.length);
+                            }
+                            createRectangle();
+                        }
+
+                        createShape();
+                    });
+
                 }
             }]
         }
